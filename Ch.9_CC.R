b8a0b76ddba9aa4dbc57d472dfbf0543c6341af7
cchambe12 <cchamberlain@g.harvard.edu>
2017-10-31 13:29:18 -0400

    old homework answer from last year

---
+++ Ch.9_CC.R
@@ -0,0 +1,13 @@
+# Practice Problems for Gelman and Hill 
+# Chapter 9: #1 - Cat
+
+# 1
+# Ti -> 1 = schools with vending machines
+#       0 = schools without vending machines
+# i = children
+# yi = B0 + B1Ti + B2xi + error
+# xi = weight/health prior to going to school
+# There would have to be an even number of students spread between 0 and 1 schools
+# and they should be randomly selected. This is assuming schools have similar demographics
+# and that there is an even vending machine to child ratio.
+
